bee7bdc15084be36db725037206a7a51ee326d79
Lohayna Lopes Machado <lohayna@gisis.local>
2026-04-07 17:47:34 -0300

    mais proj

--- wave_1D.py
+++ wave_1D.py
@@ -1,3 +1,29 @@
 #wave propagation 1D
+##bibliotecas
 
-def wave(x, t, v, A, L)
+import numpy as np
+import matplotlib.pyplot as plt
+
+#criar modelo
+
+
+
+#criar geometria
+
+
+
+#ricker
+
+def f(t,fm):
+
+ return (1-2*(np.pi*t*fm)**2)*np.exp(-(np.pi*t*fm)**2)
+
+fm = 5
+t = np.linspace(-1, 1, 1000)
+
+ricker = f(t, fm)
+
+#DIFERENÇAS FINITAS
+
+def wave(x, t, v, A, L):
+    
