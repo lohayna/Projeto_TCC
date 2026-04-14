e20374c891a3ff675e162aa11a5c520418e72f69
Lohayna Lopes Machado <lohayna@gisis.local>
2026-04-14 15:47:44 -0300

    .

--- wave_1D.py
+++ wave_1D.py
@@ -5,13 +5,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #criar modelo
+n = 1000
+velocity = np.zeros(n)
+velocity[0:400] = 2.3
+velocity[400:800] = 2.6
+velocity[800:1000] = 2.9
 
+#plt.plot(np.arange(n), velocity)
+plt.plot(velocity, np.arange(n))
 
+plt.gca().invert_yaxis()
+plt.show()
 
 #criar geometria
+dx = 5
+dz = 5
+nx = 200
+nz = 200
 
-
-
+sx = 0
+sz = 1
 #ricker
 
 def f(t,fm):
@@ -25,5 +38,5 @@ ricker = f(t, fm)
 
 #DIFERENÇAS FINITAS
 
-def wave(x, t, v, A, L):
+#def wave(x, t, v, A, L):
     
